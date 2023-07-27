99e8801351aa66de800da733b85ecf6429f26036
Divyansh Pratap <dipratap@Divyanshs-MacBook-Pro.local>
2023-07-27 12:16:07 +0530

    temp changes remove

--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,6 @@
 export function isScreensPlayer() {
   const userAgent = window.navigator.userAgent.toLowerCase();
-  return true;
-  // return userAgent.includes('tizen') || userAgent.includes('electron') || userAgent.includes('cros');
+  return userAgent.includes('tizen')
+      || userAgent.includes('electron')
+      || userAgent.includes('cros');
 }
