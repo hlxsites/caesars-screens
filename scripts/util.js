1aecc614decf8c264b393c5a4274d5194ffe1693
Divyansh Pratap <dipratap@adobe.com>
2023-08-02 13:23:35 +0530

    refactor

--- scripts/util.js
+++ scripts/util.js
@@ -1,10 +1,10 @@
 const PLAYER_DEBUG_MODE_ENABLED = 'player-debug-mode-enabled';
 
 export default function isScreensPlayer() {
-  return true;
-  // const userAgent = window.navigator.userAgent.toLowerCase();
-  // return userAgent.includes('tizen')
-  //     || userAgent.includes('electron')
-  //     || userAgent.includes('cros')
-  //     || localStorage.getItem(PLAYER_DEBUG_MODE_ENABLED) === 'true';
+  // return true;
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return userAgent.includes('tizen')
+      || userAgent.includes('electron')
+      || userAgent.includes('cros')
+      || localStorage.getItem(PLAYER_DEBUG_MODE_ENABLED) === 'true';
 }
